c5bd974366dc33e8a9316a44a59860161c3e1ab6
Jatinderpal singh <jatinderkpt@gmail.com>
2023-05-11 21:43:20 +0530

    ScriptableObjects/

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] Transform counterTopPoint;
     public void Interact()
     {
-        Debug.Log("Interact!");
+        Transform tomatoTranform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        tomatoTranform.localPosition = Vector3.zero;
+        Debug.Log("Interact! " + tomatoTranform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+
     }
 }
